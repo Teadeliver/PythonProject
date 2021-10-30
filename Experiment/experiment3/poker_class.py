45f4b40b1b8077b03f2282bf6bee5b7623ab6670
LittleStar <192354134@qq.com>
2021-10-30 11:22:37 +0800

    实验三

--- Experiment/experiment3/poker_class.py
+++ Experiment/experiment3/poker_class.py
@@ -4,19 +4,14 @@ class Card:
     SUITS = ['梅花', '方片', '红桃', '黑桃']
 
     # _init__()方法是一种特殊的方法，就是Java里的构造函数
-    def __init__(self, rank, suit, face_up=True):  # self代表类的实例，而非类，类似于Java的This
+    def __init__(self, rank, suit):  # self代表类的实例，而非类，类似于Java的This
         self.rank = rank  # 牌面数字1~13
         self.suit = suit  # 花色
-        self.is_face_up = face_up  # 是否显示牌的正面，True为正面，False为反面
 
     def __str__(self):  # 重写print()方法，打印一张牌的信息
-        if self.is_face_up:
-            rep = self.suit + self.rank
-        else:
-            rep = 'XX'
-        return rep
+        return self.suit + self.rank
 
-    def pic_order(self):  # 牌的顺序号
+    def pic_order(self):  # 牌的顺序号,将牌面转换为数字顺序号
         if self.rank == 'A':
             face_num = 1
         elif self.rank == 'J':
@@ -36,9 +31,6 @@ class Card:
         else:
             suit = 4
         return (suit - 1) * 13 + face_num
-
-    def flip(self):  # 翻牌方法
-        self.is_face_up = not self.is_face_up
 
 
 # Hand类：一手牌
@@ -62,11 +54,6 @@ class Hand:
     def add(self, card):  # 增加手里的牌
         self.cards.append(card)
 
-    def give(self, card, other_hand):  # 把一张牌给其他选手
-        self.cards.remove(card)
-        other_hand.add(card)
-        # other_hand.append(card)   	# 上面两行可以用这一行代替
-
 
 # Poke类：一副牌
 # 继承Hand类
@@ -74,7 +61,7 @@ class Poke(Hand):
     """Poke类代表一副牌，可以看做是有52张牌的牌手，所以继承Hand类。由于其中cards列表变量要存储52张牌
     而且要发牌，洗牌，所以增加方法如下方法:"""
 
-    def populate(self):  # 生成一副牌
+    def populate(self):  # 生成一副牌,双循环按顺序生成，后续进行打乱
         for suit in Card.SUITS:
             for rank in Card.RANKS:
                 self.add(Card(rank, suit))
@@ -85,26 +72,23 @@ class Poke(Hand):
 
     def deal(self, hands, per_hand=13):  # 将牌发给玩家，每人默认13张牌
         for rounds in range(per_hand):
-            for hand in hands:
-                if self.cards:
-                    top_card = self.cards[0]
-                    self.cards.remove(top_card)
-                    hand.add(top_card)
-                    # self.give(top_card,hand)	#上两句可以用此句替换
+            for hand_x in hands:  # 四个牌手
+                if self.cards:  # 判断是不是还有牌
+                    top_card = self.cards[0]  # 类似于栈，读取顶上的一张牌
+                    self.cards.remove(top_card)  # 移除顶上的一张牌
+                    hand_x.add(top_card)  # 将顶上的牌给牌手
                 else:
                     print('不能继续发牌了，牌已经发完了!')
 
 
-"""
-if __name__ == '__main__':的作用
-一个python文件通常有两种使用方法，
-第一是作为脚本直接执行，
-第二是 import 到其他的 python 脚本中被调用（模块重用）执行。
-因此 if __name__ == 'main': 的作用就是控制这两种情况执行代码的过程，
-在 if __name__ == 'main': 下的代码只有在第一种情况下（即文件作为脚本直接执行）才会被执行，
-而 import 到其他脚本中是不会被执行的。
-"""
-if __name__ == "__main__":
+# if __name__ == '__main__':的作用
+# 一个python文件通常有两种使用方法，
+# 第一是作为脚本直接执行，
+# 第二是 import 到其他的 python 脚本中被调用（模块重用）执行。
+# 因此 if __name__ == 'main': 的作用就是控制这两种情况执行代码的过程，
+# 在 if __name__ == 'main': 下的代码只有在第一种情况下（即文件作为脚本直接执行）才会被执行，
+# 而 import 到其他脚本中是不会被执行的。
+if __name__ == "__main__":  # 直接执行的话不用这句话也没事
     print('This is a module with classes for playing cards.')
     players = [Hand(), Hand(), Hand(), Hand()]
     poke = Poke()
